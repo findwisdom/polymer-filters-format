e7d212868bef4a2422f2faebc1f0295766b5efe6
e <826206931@qq.com>
2018-10-25 16:09:25 +0800

    webpack添加打包完成

---
+++ test.js
@@ -0,0 +1,2 @@
+const xx = require('./dist/index.js')
+console.log(xx)
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,27 @@
 const path = require('path');
 
 module.exports = {
+    mode:'production',
     entry: ["./src/index.js"],
     output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist/',
         filename: 'index.js',
-        path: path.resolve(__dirname, 'dist')
+        library: 'filterSrc',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    
+        // filename: 'index.js',
+        // path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
                 use: {
                     loader: 'babel-loader'
-                },
-                exclude: '/node_modules/'
+                }
             }
         ]
     }
